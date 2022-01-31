039704951b8ebeeb702132685e3c76acf6523f63
luke-h1 <luke.howsam@yahoo.com>
2022-01-31 11:57:04 +0000

    setup view methods

--- posts/models.py
+++ posts/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from datetime import datetime 
 from author.models import Author
 
-class Posts(models.Model):
+class Post(models.Model):
   author = models.ForeignKey(Author, on_delete=models.DO_NOTHING)
   title = models.CharField(max_length=80)
   intro = models.CharField(max_length=80)
--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 
-# Create your views here.
+from .models import Post
+
+def index(request):
+  
+  posts = Post.objects.order_by('-publish_date').filter(is_published=True)
+  
+  paginator = Paginator(posts, 6)
+  
+  # get param from URL
+  page = request.GET.get('page')
+  paged_posts = paginator.get_page(page)
+  
+  context = {
+    'posts': paged_posts
+  }
+  
+  return render(request, 'posts/posts.html', context)
+
+
+def post(request, post_id):
+  post = get_object_or_404(Post, pk=post_id).filter(is_published=True)
+  context = {
+    'post': post
+  }
+  return render(request, 'posts/post.html', context)
+
